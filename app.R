d11d67df7551414c05abd1a5623ea5b381da7e31
Corey Teply <73682551+teplycj@users.noreply.github.com>
2022-03-26 16:58:41 -0700

    Add files via upload

--- app.R
+++ app.R
@@ -1,3 +1,6 @@
+#copyright - Corey Teply of Aslan Brewing Co. Bellingham, WA - 2022
+#R-Shiny app to display all locations our beers are located outside of our three retail locations
+
 require(shiny)
 require(shinyWidgets)
 require(leaflet)
