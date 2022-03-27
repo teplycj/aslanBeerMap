db5a4e6a46fc17b5c69c4b4bb7fce15f4dc26edc
Corey Teply <73682551+teplycj@users.noreply.github.com>
2022-03-26 17:06:40 -0700

    Add files via upload

--- downloadLibs.R
+++ downloadLibs.R
@@ -1,9 +1,11 @@
 mapPackages <- c("shiny","shinyWidgets","leaflet","stringr") #packages that my program depends on
 machinePackages <- installed.packages() #all packages downloaded on your local machine
 
+#check to see if the packages needed for my app are already on your machine, and if not, download them!
 for(p in mapPackages){
   if(!(p %in% rownames(machinePackages))){
     print(paste("downloading:", p))
-    install.packages(p)
+    #UNCOMMENT THE INSTALL.PACKAGES(P) LINE TO ACUTALLY DOWNLOAD THEM!
+    #install.packages(p) 
   }
 }
